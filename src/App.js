9b73f59e876490f5aa5cfe7b1cd127683fcf667b
LipzDev <filipe.gt1998@outlook.com>
2021-04-25 17:53:38 -0300

    Criado a sessão background

--- src/App.js
+++ src/App.js
@@ -8,6 +8,59 @@ const App = () => {
     <>
       <Header />
       <Bg />
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non error
+        possimus et inventore labore, dignissimos aperiam a dolor commodi magni.
+        Nemo hic ipsam soluta consectetur magni nam dolore incidunt
+        aperiam.Lorem ipsum dolor sit amet consectetur adipisicing elit. Non
+        error possimus et inventore labore, dignissimos aperiam a dolor commodi
+        magni. Nemo hic ipsam soluta consectetur magni nam dolore incidunt
+        aperiam.Lorem ipsum dolor sit amet consectetur adipisicing elit. Non
+        error possimus et inventore labore, dignissimos aperiam a dolor commodi
+        magni. Nemo hic ipsam soluta consectetur magni nam dolore incidunt
+        aperiam.Lorem ipsum dolor sit amet consectetur adipisicing elit. Non
+        error possimus et inventore labore, dignissimos aperiam a dolor commodi
+        magni. Nemo hic ipsam soluta consectetur magni nam dolore incidunt
+        aperiam.Lorem ipsum dolor sit amet consectetur adipisicing elit. Non
+        error possimus et inventore labore, dignissimos aperiam a dolor commodi
+        magni. Nemo hic ipsam soluta consectetur magni nam dolore incidunt
+        aperiam.Lorem ipsum dolor sit amet consectetur adipisicing elit. Non
+        error possimus et inventore labore, dignissimos aperiam a dolor commodi
+        magni. Nemo hic ipsam soluta consectetur magni nam dolore incidunt
+        aperiam.incidunt aperiam.Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Non error possimus et inventore labore, dignissimos
+        aperiam a dolor commodi magni. Nemo hic ipsam soluta consectetur magni
+        nam dolore incidunt aperiam.incidunt aperiam.Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Non error possimus et inventore labore,
+        dignissimos aperiam a dolor commodi magni. Nemo hic ipsam soluta
+        consectetur magni nam dolore incidunt aperiam.incidunt aperiam.Lorem
+        ipsum dolor sit amet consectetur adipisicing elit. Non error possimus et
+        inventore labore, dignissimos aperiam a dolor commodi magni. Nemo hic
+        ipsam soluta consectetur magni nam dolore incidunt aperiam.incidunt
+        aperiam.Lorem ipsum dolor sit amet consectetur adipisicing elit. Non
+        error possimus et inventore labore, dignissimos aperiam a dolor commodi
+        magni. Nemo hic ipsam soluta consectetur magni nam dolore incidunt
+        aperiam.incidunt aperiam.Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Non error possimus et inventore labore, dignissimos
+        aperiam a dolor commodi magni. Nemo hic ipsam soluta consectetur magni
+        nam dolore incidunt aperiam.incidunt aperiam.Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Non error possimus et inventore labore,
+        dignissimos aperiam a dolor commodi magni. Nemo hic ipsam soluta
+        consectetur magni nam dolore incidunt aperiam.incidunt aperiam.Lorem
+        ipsum dolor sit amet consectetur adipisicing elit. Non error possimus et
+        inventore labore, dignissimos aperiam a dolor commodi magni. Nemo hic
+        ipsam soluta consectetur magni nam dolore incidunt aperiam.incidunt
+        aperiam.Lorem ipsum dolor sit amet consectetur adipisicing elit. Non
+        error possimus et inventore labore, dignissimos aperiam a dolor commodi
+        magni. Nemo hic ipsam soluta consectetur magni nam dolore incidunt
+        aperiam.incidunt aperiam.Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Non error possimus et inventore labore, dignissimos
+        aperiam a dolor commodi magni. Nemo hic ipsam soluta consectetur magni
+        nam dolore incidunt aperiam.incidunt aperiam.Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Non error possimus et inventore labore,
+        dignissimos aperiam a dolor commodi magni. Nemo hic ipsam soluta
+        consectetur magni nam dolore incidunt aperiam.
+      </h1>
     </>
   );
 };
--- src/Components/Background/Bg.js
+++ src/Components/Background/Bg.js
@@ -2,7 +2,29 @@ import React from 'react';
 import './Bg.css';
 
 const Bg = () => {
-  return <section className="background"></section>;
+  const [pos, setPos] = React.useState(0);
+
+  React.useEffect(() => {
+    window.addEventListener('scroll', () => {
+      setPos(window.scrollY);
+    });
+  }, [pos]);
+
+  return (
+    <section className="background">
+      <div className="container">
+        <div
+          style={pos > 350 ? { opacity: '0' } : { opacity: '1' }}
+          className="background__item"
+        >
+          <h1 style={{ transform: `translate(${pos}px, 0)` }}>REACT.JS</h1>
+          <h2 style={{ transform: `translate(-${pos}px, 0)` }}>
+            A Biblioteca dos sonhos
+          </h2>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Bg;
