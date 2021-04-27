5d8006008ed29c6285db002f3ec495f622d87544
LipzDev <filipe.gt1998@outlook.com>
2021-04-27 17:52:00 -0300

    footer

--- src/App.js
+++ src/App.js
@@ -5,15 +5,19 @@ import Bg from './Components/Background/Bg';
 import About from './Components/About/About';
 import Parallax from './Components/Parallax/Parallax';
 import More from './Components/More/More';
+import Footer from './Components/Footer/Footer';
 
 const App = () => {
   return (
     <>
       <Header />
-      <Bg />
-      <About />
-      <Parallax />
-      <More />
+      <main>
+        <Bg />
+        <About />
+        <Parallax />
+        <More />
+      </main>
+      <Footer />
     </>
   );
 };
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="footer__item">
+          <a href="https://github.com/LipzDev">
+            Todos os direitos reservados - <span>LipzDev</span>
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
